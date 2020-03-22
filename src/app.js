824d78e345055d6c7fd45f93f1388df543d6d3ee
Abhinav Pandey <abhinavpandey.1116@gmail.com>
2020-03-22 19:51:25 -0400

    change_done_for_deployment

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ const express = require('express'); //express is the function and not object
 const hbs = require('hbs')
 const geocode = require('../utils/geocode');
 const forecast = require('../utils/forecast');
+const port = process.env.PORT || 3000
 
 // console.log(__dirname);
 // console.log(path.join(__dirname,'../public')) // Navigating to correct path for html
@@ -114,6 +115,6 @@ app.get('*',(req,res) => {
     })
 })
 
-app.listen(3000,()=>{
-    console.log('Server is up on port 3000');
+app.listen(port,()=>{
+    console.log('Server is up on port' + port);
 });
